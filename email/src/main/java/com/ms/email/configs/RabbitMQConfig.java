5571c1ece5cdf20d3b870f05e94814ee9c8d7aee
Stefano Paulo <stefanopaulo47@gmail.com>
2025-11-08 18:19:08 -0300

    Finalizando o Consumer do RabbitMQ

--- email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -1,7 +1,10 @@
 package com.ms.email.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,7 +12,13 @@ public class RabbitMQConfig {
     @Value("${broker.queue.email.name}")
     private String queue;
 
+    @Bean
     public Queue queue() {
         return new Queue(queue, true);
+    }
+
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
