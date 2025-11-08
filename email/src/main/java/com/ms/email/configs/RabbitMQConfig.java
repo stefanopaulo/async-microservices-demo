1302a4e3cb556c44ed497f81e69226bed7aed271
Stefano Paulo <stefanopaulo47@gmail.com>
2025-11-08 20:15:55 -0300

    Implementando envio de emails com o SMTP do Gmail

--- email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -17,6 +17,7 @@ public class RabbitMQConfig {
         return new Queue(queue, true);
     }
 
+    @Bean
     public Jackson2JsonMessageConverter messageConverter() {
         ObjectMapper objectMapper = new ObjectMapper();
         return new Jackson2JsonMessageConverter(objectMapper);
---
+++ email/src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,54 @@
+package com.ms.email.services;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
+    private final EmailRepository emailRepository;
+    private final JavaMailSender emailSender;
+
+    public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
+        this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel emailModel) {
+        try {
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(emailFrom);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+            return emailRepository.save(emailModel);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+            log.error("Erro ao enviar email {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Erro inesperado ao salvar email {}", e.getMessage());
+        }
+
+        return emailModel;
+    }
+}
